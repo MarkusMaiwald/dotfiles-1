3acc3251ca0e8fbc5653a07279fde14a01c79b1a
Will St. Clair <will@willstclair.com>
2011-06-08 13:54:52 -0500

    Refactor git functions and prompt

--- zsh/lib/git.zsh
+++ zsh/lib/git.zsh
@@ -1,101 +1,16 @@
-function current-branch() {
+function git-current-branch() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
   echo ${ref#refs/heads/}
 }
 
 function git-track() {
-  branch=$(current-branch)
+  branch=$(git-current-branch)
   git config branch.$branch.remote origin
   git config branch.$branch.merge refs/heads/$branch
   echo "tracking origin/$branch"
-}
-
-# get the name of the branch we are on
-function git-prompt-info() {
-  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse-git-dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
-}
-
-# Checks if working tree is dirty
-parse-git-dirty() {
-  if [[ -n $(git status -s 2> /dev/null) ]]; then
-    echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
-  else
-    echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
-  fi
-}
-
-# Checks if there are commits ahead from remote
-function git-prompt-ahead() {
-  if $(echo "$(git log @{upstream}..HEAD 2> /dev/null)" | grep '^commit' &> /dev/null); then
-    echo "$ZSH_THEME_GIT_PROMPT_AHEAD"
-  fi
-}
-
-# Checks if we are behind remote
-function git-prompt-tracking() {
-  behind=$(git rev-list ^HEAD @{upstream} 2> /dev/null | wc -l)
-  ahead=$(git log @{upstream}..HEAD --format='%H' 2> /dev/null | wc -l)
-  if [[ $behind -gt 0 && $ahead -gt 0 ]]; then
-    echo "$ZSH_THEME_GIT_PROMPT_DIVERGED"
-  elif [[ $behind -gt 0 ]]; then
-    echo "$ZSH_THEME_GIT_PROMPT_BEHIND"
-  elif [[ $ahead -gt 0 ]]; then
-    echo "$ZSH_THEME_GIT_PROMPT_AHEAD"
-  fi
-
-  branch=$(current-branch)
-  remote=$(git config --get branch.$branch.remote)
-  merge=$(git config --get branch.$branch.merge)
-  if [[ -z $remote || -z $merge ]]; then
-    echo $ZSH_THEME_GIT_NO_TRACK
-  fi
 }
 
 function git-can-ff() {
   a="$(git rev-parse "$1")" &&
   test "$(git merge-base "$a" "$2")" = "$a"
 }
-
-# Formats prompt string for current git commit short SHA
-function git-prompt-short-sha() {
-  SHA=$(git rev-parse --short HEAD 2> /dev/null) && echo "$ZSH_THEME_GIT_PROMPT_SHA_BEFORE$SHA$ZSH_THEME_GIT_PROMPT_SHA_AFTER"
-}
-
-# Formats prompt string for current git commit long SHA
-function git-prompt-long-sha() {
-  SHA=$(git rev-parse HEAD 2> /dev/null) && echo "$ZSH_THEME_GIT_PROMPT_SHA_BEFORE$SHA$ZSH_THEME_GIT_PROMPT_SHA_AFTER"
-}
-
-# Get the status of the working tree
-git-prompt-status() {
-  INDEX=$(git status --porcelain 2> /dev/null)
-  STATUS=""
-  if $(echo "$INDEX" | grep '^?? ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_UNTRACKED$STATUS"
-  fi
-  if $(echo "$INDEX" | grep '^A  ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_ADDED$STATUS"
-  elif $(echo "$INDEX" | grep '^M  ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_ADDED$STATUS"
-  fi
-  if $(echo "$INDEX" | grep '^ M ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
-  elif $(echo "$INDEX" | grep '^AM ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
-  elif $(echo "$INDEX" | grep '^ T ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
-  fi
-  if $(echo "$INDEX" | grep '^R  ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_RENAMED$STATUS"
-  fi
-  if $(echo "$INDEX" | grep '^ D ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_DELETED$STATUS"
-  fi
-  if $(echo "$INDEX" | grep '^UU ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_UNMERGED$STATUS"
-  fi
-  echo $STATUS
-}
-
-
--- zsh/lib/prompt.zsh
+++ zsh/lib/prompt.zsh
@@ -1,6 +1,6 @@
 setopt prompt_subst
 
-function zsh-prompt-os() {
+function prompt-os() {
   case $(uname) in
     (Darwin)
       echo $ZSH_THEME_OS_DARWIN;
@@ -11,11 +11,45 @@ function zsh-prompt-os() {
   esac
 }
 
-function zsh-prompt-vagrant() {
+function prompt-vagrant() {
   if [[ -f ".vagrant" ]]; then
     echo $ZSH_THEME_VAGRANT;
   fi
+}
 
+function prompt-git() {
+  if git rev-parse --git-dir > /dev/null 2>&1; then
+    echo "$(prompt-git-branch)$(prompt-git-dirty)$(prompt-git-tracking)"
+  fi
+}
+
+function prompt-git-branch() {
+  echo "$ZSH_THEME_GIT_PROMPT_PREFIX$(git-current-branch)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+}
+
+function prompt-git-dirty() {
+  if [[ -n $(git status -s 2> /dev/null) ]]; then
+    echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
+  fi
+}
+
+function prompt-git-tracking() {
+  branch=$(git-current-branch)
+  remote=$(git config --get branch.$branch.remote)
+  merge=$(git config --get branch.$branch.merge)
+  if [[ -z $remote || -z $merge ]]; then
+    echo $ZSH_THEME_GIT_NO_TRACK
+  else
+    behind=$(git rev-list ^HEAD @{upstream} 2> /dev/null | wc -l)
+    ahead=$(git log @{upstream}..HEAD --format='%H' 2> /dev/null | wc -l)
+    if [[ $behind -gt 0 && $ahead -gt 0 ]]; then
+      echo "$ZSH_THEME_GIT_PROMPT_DIVERGED"
+    elif [[ $behind -gt 0 ]]; then
+      echo "$ZSH_THEME_GIT_PROMPT_BEHIND"
+    elif [[ $ahead -gt 0 ]]; then
+      echo "$ZSH_THEME_GIT_PROMPT_AHEAD"
+    fi
+  fi
 }
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%F{yellow}"
@@ -26,10 +60,11 @@ ZSH_THEME_GIT_PROMPT_AHEAD=" %F{green}→%f"
 ZSH_THEME_GIT_PROMPT_BEHIND=" %F{red}←%f"
 ZSH_THEME_GIT_PROMPT_DIVERGED=" %F{yellow}⇆%f"
 ZSH_THEME_GIT_NO_TRACK=" %F{red}⚡%f"
+
 ZSH_THEME_VAGRANT="❒ "
+
 ZSH_THEME_OS_DARWIN=" "
 ZSH_THEME_OS_LINUX="ℓ "
 
-PROMPT='%F{green}%2c%F{blue} $(zsh-prompt-os)[%f '
-RPROMPT='$(git-prompt-info)$(git-prompt-tracking) %F{blue}$(zsh-prompt-vagrant)] %F{green}%D{%L:%M} %F{yellow}%D{%p}%f'
-
+PROMPT='%F{green}%2c%F{blue} $(prompt-os)[%f '
+RPROMPT='$(prompt-git) %F{blue}$(prompt-vagrant)] %F{green}%D{%L:%M} %F{yellow}%D{%p}%f'
